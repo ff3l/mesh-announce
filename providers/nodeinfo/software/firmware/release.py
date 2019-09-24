3f4c94b622e1082a5720e93237e7c3c9826ca451
Freifunk 3Laendereck <admin@freifunk-3laendereck.net>
2019-09-24 12:02:35 +0200

    "Firmware": show full Debian release

--- providers/nodeinfo/software/firmware/release.py
+++ providers/nodeinfo/software/firmware/release.py
@@ -3,4 +3,4 @@ from providers.util import call
 
 class Source(providers.DataSource):
     def call(self):
-        return call(['lsb_release','-rs'])[0]
+        return call(['lsb_release','-ds'])[0]
